a86062c1f717518aa8430d0b185f5d120e625a3c
Benjamint <BFJT.vandenTillaart@student.han.nl>
2025-01-17 13:07:59 +0100

    Big fix
    cleanup

--- js/navigation.js
+++ js/navigation.js
@@ -3,57 +3,48 @@ document.getElementById('navigation').innerHTML = `
   <ul class="nav">
     <li><a href="#" data-page="html/home.html"><i class="fa fa-home"></i>Home</a></li>
     <li><a href="#" data-page="html/about.html"><i class="fa fa-building"></i>Elk Achtergrond</a></li>
-
-    <li><a href="#" data-page="html/services.html"><i class="fa fa-map"></i>Wijkscan</a></li>
-    
     <li class="has-submenu">
        <a href="#" data-page="html/portfolio.html"><i class="fa fa-comments"></i>Portfolio groep elk</a>
        <ul class="submenu always-visible">
-          <a href="#" data-page="html/benjaminPortfolio.html"><i class="fa fa-user"></i>Benjamin</a>
-          <a href="#" data-page="html/robinPortfolio.html"><i class="fa fa-user"></i>Robin</a>
-          <a href="#" data-page="html/stijnPortfolio.html"><i class="fa fa-user"></i>Stijn</a>
+          <li><a href="#" data-page="html/benjaminPortfolio.html"><i class="fa fa-user"></i>Benjamin</a></li>
+          <li><a href="#" data-page="html/robinPortfolio.html"><i class="fa fa-user"></i>Robin</a></li>
+          <li><a href="#" data-page="html/stijnPortfolio.html"><i class="fa fa-user"></i>Stijn</a></li>
           <ul class="submenu always-visible">
-          <a href="#" data-page="html/Api's/weerAPI.html"><i class="fa fa-user"></i>Weer</a>
-          <a href="#" data-page="html/Api's/nasa.html"><i class="fa fa-user"></i>Nasa</a>
+            <li><a href="#" data-page="html/apis/weerAPI.html"><i class="fa fa-user"></i>Weer</a></li>
+            <li><a href="#" data-page="html/apis/nasa.html"><i class="fa fa-user"></i>Nasa</a></li>
           </ul>
         </ul>
     </li>
-
   </ul>
 `;
 
 function loadContent(page) {
   fetch(page)
     .then(response => {
-      if (!response.ok) throw new Error(`Error loading ${page}`);
+      if (!response.ok) throw new Error(`Fout bij het laden van: ${page}`);
       return response.text();
     })
     .then(html => {
       document.getElementById('content').innerHTML = html;
-
-      // Scripts opnieuw uitvoeren
-      document.querySelectorAll('#content script').forEach(script => {
-        const newScript = document.createElement('script');
-        newScript.textContent = script.textContent;
-        document.body.appendChild(newScript);
-        script.remove();
-      });
     })
     .catch(error => {
       console.error(error);
-      document.getElementById('content').innerHTML = '<p>Failed to load content.</p>';
+      document.getElementById('content').innerHTML = `
+        <p>De pagina kon niet worden geladen. Controleer of het bestand bestaat en probeer opnieuw.</p>
+      `;
     });
 }
-
 
 // Laad standaard de Home-sectie
 loadContent('html/home.html');
 
 // Voeg klikgebeurtenis toe aan navigatielinks
-document.querySelectorAll('#navigation a').forEach(link => {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    const page = this.getAttribute('data-page');
-    loadContent(page);
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('#navigation a').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const page = this.getAttribute('data-page');
+      loadContent(page);
+    });
   });
 });
